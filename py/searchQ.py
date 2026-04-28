1e60d2e1259a569de915d227f0ea47851fde4de3
MM33366 <emma65454@gmail.com>
2026-04-28 12:15:09 +0800

    movie2

---
+++ py/searchQ.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+…
+@app.route("/searchQ", methods=["POST","GET"])
+def searchQ():
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        info = ""
+        db = firestore.client()     
+        collection_ref = db.collection("電影")
+        docs = collection_ref.order_by("showDate").get()
+        for doc in docs:
+            if MovieTitle in doc.to_dict()["title"]: 
+                info += "片名：" + doc.to_dict()["title"] + "<br>" 
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+        return info
+    else:  
+        return render_template("input.html")
